a0fbef9dc5316e56dbad5786e091950d1fe9faef
Gabriel Ratener <gabe9693@gmail.com>
2019-07-09 12:47:03 -0600

    Autofind active interface for Mac

--- src/mac-connect.js
+++ src/mac-connect.js
@@ -1,28 +1,51 @@
-var exec = require('child_process').exec;
-var env = require('./env');
+const {exec} = require('child_process');
+const env = require('./env');
+
+const getIface = () => {
+  const command = "networksetup -listallhardwareports | awk '/^Hardware Port: (Wi-Fi|AirPort)$/{getline;print $2}'";
+
+  return new Promise((win, fail) => {
+    exec(command, env, (err, str) => {
+      if (err)
+        fail(err);
+      else
+        win(str.trim());
+    })
+  })
+}
 
 function connectToWifi(config, ap, callback) {
 
-  var iface = 'en0';
-  var commandStr = "networksetup -setairportnetwork ";
+  const doItNow = (iface) => {
+    // because we possibly have to determine the interface before...
+    var commandStr = "networksetup -setairportnetwork ";
 
-  if (config.iface) {
-      iface = config.iface.toString();
+    commandStr = commandStr + "'" + iface + "'" + " " + "'" + ap.ssid + "'" + " " + "'" + ap.password + "'";
+    //console.log(commandStr);
+  
+    exec(commandStr, env, function(err, resp, stderr) {
+      //console.log(stderr, resp);
+      if (resp && resp.indexOf('Failed to join network') >= 0) {
+        callback && callback(resp);
+      } else if (resp && resp.indexOf('Could not find network') >= 0) {
+        callback && callback(resp);
+      } else {
+        callback && callback(err);
+      }
+    });
   }
 
-  commandStr = commandStr + "'" + iface + "'" + " " + "'" + ap.ssid + "'" + " " + "'" + ap.password + "'";
-  //console.log(commandStr);
-
-  exec(commandStr, env, function(err, resp, stderr) {
-    //console.log(stderr, resp);
-    if (resp && resp.indexOf('Failed to join network') >= 0) {
-      callback && callback(resp);
-    } else if (resp && resp.indexOf('Could not find network') >= 0) {
-      callback && callback(resp);
-    } else {
-      callback && callback(err);
-    }
-  });
+  if (config.iface) {
+    doItNow(config.iface.toString())
+  } else {
+    getIface()
+      .then((iface) => {
+        doItNow(iface);
+      }, (err) => {
+        if (callback)
+          callback(err)
+      })
+  }
 }
 
 module.exports = function (config) {
